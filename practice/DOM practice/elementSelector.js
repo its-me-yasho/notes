efa7302ba878da03d9671ba1012456d17888dfab
Yashodhan-Chaudhari <yashodhan.chaudhari@emtecinc.com>
2022-09-02 19:26:18 +0530

    second commit

---
+++ practice/DOM practice/elementSelector.js
@@ -0,0 +1,26 @@
+// DOM Manipulation
+
+// getElementById()
+// const id = document.getElementById('h1')
+// console.log(id)
+
+
+// getElementByClassName()
+// const className = document.getElementsByClassName('list-items')
+// console.log(className)
+
+// getElementByTagName()
+// const tagName = document.getElementsByTagName('div')[0]
+// console.log(tagName)
+
+// querySelector()
+// const querySel = tagName.querySelector('ol')
+// console.log(querySel)
+
+// const querySel1 = document.querySelector('#heading1')
+// console.log(querySel1)
+
+// querySelectorAll()
+// const selAll = document.querySelectorAll('.list-items')
+// console.log(selAll)
+
